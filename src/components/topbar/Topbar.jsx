23b137f35e56e04f68e2f2a075b6d6167aefcb20
chhabra9 <achintsinghec19@acropolis.in>
2021-12-10 01:12:51 +0530

    complete the navigation bar and implent the hamburger menu

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,9 +1,10 @@
-import React from 'react'
-import "./topbar.scss"
-import {Person,Mail} from "@material-ui/icons"
-export default function Topbar() {
+import React from 'react';
+import "./topbar.scss";
+import {Person,Mail} from "@material-ui/icons";
+export default function Topbar({menuOpen, setMenuOpen}) {
     return (
-        <div className='topbar'>
+        
+        <div className={'topbar '+(menuOpen && "active")}>
            <div className='wrapper'>
           <div className="left">
              <a href='#intro' className='logo'>Achint Singh</a>
@@ -17,6 +18,12 @@ export default function Topbar() {
              </div>
           </div>
           <div className="right">
+          <div className='hamburger' onClick={()=>setMenuOpen(!menuOpen)}>
+                    <span className='line1'></span>
+                    <span className='line2'></span>
+                    <span className='line3'></span>
+
+              </div>
           </div>
           </div>
         </div>
