2d1acce338cd673682de4f260f01ff5fd021dc8d
chhabra9 <achintsinghec19@acropolis.in>
2021-12-12 01:15:49 +0530

    complete the testimonial page

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -2,7 +2,12 @@ import "./contact.scss"
 export default function Contact() {
     return (
         <div className="contact" id="contact">
-            
+            <div className="left">
+                <img src="assets/shake.svg"/>
+            </div>
+            <div className="right">
+                <h2>Contact.</h2>
+            </div>
         </div>
     )
 }
